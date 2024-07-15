f037f439913c222abfeadcbad7860f4d073f4371
techstarteranhkim <anh.kim@docc.techstarter.de>
2024-07-15 09:05:54 +0200

    routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import TodosRouter from "./todos";
+import UserRouter from "./users";
 
 const AppRouter = Router();
 
 AppRouter.use("/todos", TodosRouter);
+AppRouter.use("/user", UserRouter);
 
 export default AppRouter;
