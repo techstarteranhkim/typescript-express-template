d7579fc557202b5011c2ff722d1e653e3f3e21ba
techstarteranhkim <anh.kim@docc.techstarter.de>
2024-07-17 13:40:36 +0200

    init

--- src/interfaces/db-models/TodoAttributes.ts
+++
@@ -1,7 +0,0 @@
-export interface TodoAttributes {
-  id?: number;
-  userId: number;
-  task: string;
-  isDone?: boolean;
-  dueDate: Date | string;
-}
--- src/interfaces/db-models/UserAttributes.ts
+++
@@ -1,7 +0,0 @@
-export interface UserAttributes {
-  id?: number;
-  email: string;
-  name: string;
-  password: string;
-  profileImgUrl?: string;
-}
